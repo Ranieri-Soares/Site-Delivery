7ac283fd8390b69d444182beb049ce289a1c3f45
Ranieri-Soares <ranieri_silvasoares@hotmail.com>
2020-10-08 23:31:19 -0300

    Atualização dos Banners

--- index.php
+++ index.php
@@ -3,51 +3,37 @@
       <!-- Swiper-->
       <section class="section swiper-container swiper-slider swiper-slider-modern" data-loop="true" data-autoplay="5000" data-simulate-touch="true" data-nav="true" data-slide-effect="fade">
         <div class="swiper-wrapper text-left">
-          <div class="swiper-slide context-dark" data-slide-bg="images/slider-1-1920x729.jpg">
+          <div class="swiper-slide context-dark" data-slide-bg="images/banner/01.jpg">
             <div class="swiper-slide-caption">
               <div class="container">
                 <div class="row justify-content-center justify-content-xxl-start">
                   <div class="col-md-10 col-xxl-6">
                     <div class="slider-modern-box">
-                      <h1 class="slider-modern-title"><span data-caption-animate="slideInDown" data-caption-delay="0">Organic Food</span></h1>
-                      <p data-caption-animate="fadeInRight" data-caption-delay="400">Herber provides local citizens and guests of our town with quality organic fruits, vegetables, and other products.</p>
-                      <div class="oh button-wrap"><a class="button button-primary button-ujarak" href="about-us.html" data-caption-animate="slideInLeft" data-caption-delay="400">Read more</a></div>
+                      <h1 class="slider-modern-title"><span data-caption-animate="slideInDown" data-caption-delay="0">Burguer Freitas</span></h1>
+                      <p data-caption-animate="fadeInRight" data-caption-delay="400">Venha conhecer nossos Hamburgues artesanais, os melhores da região!.</p>
+                      <div class="oh button-wrap"><a class="button button-primary button-ujarak" href="about-us.html" data-caption-animate="slideInLeft" data-caption-delay="400">Veja Mais</a></div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="swiper-slide context-dark" data-slide-bg="images/slider-2-1920x729.jpg">
+          <div class="swiper-slide context-dark" data-slide-bg="images/banner/02.jpg">
             <div class="swiper-slide-caption">
               <div class="container">
                 <div class="row justify-content-center justify-content-xxl-start">
                   <div class="col-md-10 col-xxl-6">
                     <div class="slider-modern-box">
-                      <h1 class="slider-modern-title"><span data-caption-animate="slideInLeft" data-caption-delay="0">Quality Control</span></h1>
-                      <p data-caption-animate="fadeInRight" data-caption-delay="400">We control the process of farming at Herber to deliver the best organic products to our customers throughout the state.</p>
-                      <div class="oh button-wrap"><a class="button button-primary button-ujarak" href="about-us.html" data-caption-animate="slideInLeft" data-caption-delay="400">Read more</a></div>
+                      <h1 class="slider-modern-title"><span data-caption-animate="slideInLeft" data-caption-delay="0">Massas</span></h1>
+                      <p data-caption-animate="fadeInRight" data-caption-delay="400">Expiremente nossas pizzas, temos vários sabores, você também pode recheá-las como preferir!.</p>
+                      <div class="oh button-wrap"><a class="button button-primary button-ujarak" href="about-us.html" data-caption-animate="slideInLeft" data-caption-delay="400">Veja Mais</a></div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="swiper-slide" data-slide-bg="images/slider-3-1920x729.jpg">
-            <div class="swiper-slide-caption">
-              <div class="container">
-                <div class="row justify-content-center justify-content-xxl-start">
-                  <div class="col-md-10 col-xxl-6">
-                    <div class="slider-modern-box">
-                      <h1 class="slider-modern-title"><span data-caption-animate="slideInDown" data-caption-delay="0">Eco-Friendly</span></h1>
-                      <p data-caption-animate="fadeInRight" data-caption-delay="400">As the leading organic farm, we maintain an eco-friendly policy of growing and selling healthy food without any additives.</p>
-                      <div class="oh button-wrap"><a class="button button-primary button-ujarak" href="about-us.html" data-caption-animate="slideInUp" data-caption-delay="400">Read more</a></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        
         </div>
         <!-- Swiper Navigation-->
         <div class="swiper-button-prev"></div>
@@ -64,10 +50,10 @@
               <article class="box-icon-ruby">
                 <div class="unit box-icon-ruby-body flex-column flex-md-row text-md-left flex-lg-column align-items-center text-lg-center flex-xl-row text-xl-left">
                   <div class="unit-left">
-                    <div class="box-icon-ruby-icon linearicons-leaf"></div>
+                    <div class="box-icon-ruby-icon linearicons-smile"></div>
                   </div>
                   <div class="unit-body">
-                    <h4 class="box-icon-ruby-title"><a href="#">Natural &amp; Organic</a></h4>
+                    <h4 class="box-icon-ruby-title"><a href="#">Sanduíches</a></h4>
                   </div>
                 </div>
               </article>
@@ -76,10 +62,10 @@
               <article class="box-icon-ruby">
                 <div class="unit box-icon-ruby-body flex-column flex-md-row text-md-left flex-lg-column align-items-center text-lg-center flex-xl-row text-xl-left">
                   <div class="unit-left">
-                    <div class="box-icon-ruby-icon linearicons-shovel"></div>
+                    <div class="box-icon-ruby-icon linearicons-pie-chart"></div>
                   </div>
                   <div class="unit-body">
-                    <h4 class="box-icon-ruby-title"><a href="#">Best Equipment</a></h4>
+                    <h4 class="box-icon-ruby-title"><a href="#">Massas e Pizzas</a></h4>
                   </div>
                 </div>
               </article>
@@ -88,10 +74,10 @@
               <article class="box-icon-ruby">
                 <div class="unit box-icon-ruby-body flex-column flex-md-row text-md-left flex-lg-column align-items-center text-lg-center flex-xl-row text-xl-left">
                   <div class="unit-left">
-                    <div class="box-icon-ruby-icon linearicons-sun"></div>
+                    <div class="box-icon-ruby-icon linearicons-coffee-cup"></div>
                   </div>
                   <div class="unit-body">
-                    <h4 class="box-icon-ruby-title"><a href="#">Dedicated Team</a></h4>
+                    <h4 class="box-icon-ruby-title"><a href="#">Drinks e Bebidas</a></h4>
                   </div>
                 </div>
               </article>
@@ -105,14 +91,9 @@
         <div class="container">
           <div class="row row-40 justify-content-center">
             <div class="col-sm-8 col-md-7 col-lg-6 wow fadeInLeft" data-wow-delay="0s">
-              <div class="product-banner"><img src="images/home-banner-1-570x715.jpg" alt="" width="570" height="715"/>
-                <div class="product-banner-content">
-                  <div class="product-banner-inner" style="background-image: url(images/bg-brush.png)">
-                    <h3 class="text-secondary-1">organic</h3>
-                    <h2 class="text-primary">Vegetables</h2>
-                  </div>
-                </div>
+              <div class="product-banner"><img src="images/banner/pizza.jpg" alt="" width="570" height="715"/>
               </div>
+              
             </div>
             <div class="col-md-5 col-lg-6">
               <div class="row row-30 justify-content-center">
@@ -205,53 +186,17 @@
 
       <!-- Section CTA 2-->
       <section class="section text-center">
-        <div class="parallax-container" data-parallax-img="images/bg-parallax-1.jpg">
+        <div class="parallax-container" data-parallax-img="images/banner/promocoes.jpg">
           <div class="parallax-content section-xl section-inset-custom-1 context-dark bg-overlay-40">
             <div class="container">
-              <h2 class="oh font-weight-normal"><span class="d-inline-block wow slideInDown" data-wow-delay="0s">Our Approach</span></h2>
-              <p class="oh big text-width-large"><span class="d-inline-block wow slideInUp" data-wow-delay=".2s">Our farm strictly combines the traditions of organic farming with the latest innovations to make our products healthy and safe for the clients.</span></p><a class="button button-primary button-icon button-icon-left button-ujarak wow fadeInUp" href="https://www.youtube.com/watch?v=-AhmuMqZB0s" data-lightgallery="item" data-wow-delay=".1s"><span class="icon mdi mdi-play"></span>View presentation</a>
+              <h2 class="oh font-weight-normal"><span class="d-inline-block wow slideInDown" data-wow-delay="0s">Promoções</span></h2>
+              <p class="oh big text-width-large"><span class="d-inline-block wow slideInUp" data-wow-delay=".2s">Veja as nossas outras promoções clicando no botão abaixo, temos várias promoções com diversos produtos.</span></p><a class="button button-primary button-icon button-icon-left button-ujarak wow fadeInUp" href="https://www.youtube.com/watch?v=-AhmuMqZB0s" data-lightgallery="item" data-wow-delay=".1s"><span class="icon mdi mdi-play"></span>Todas as Promoções</a>
             </div>
           </div>
         </div>
       </section>
 
-      <!-- Team of professionals-->
-      <section class="section section-md bg-default">
-        <div class="container">
-          <div class="oh">
-            <h2 class="wow slideInUp" data-wow-delay="0s">Our Team</h2>
-          </div>
-          <div class="row row-30 justify-content-center">
-            <div class="col-md-6 col-lg-4 col-xl-4 wow fadeInRight" data-wow-delay="0s">
-              <!-- Team Classic-->
-              <article class="team-classic"><a class="team-classic-figure" href="#"><img src="images/team-1-370x406.jpg" alt="" width="370" height="406"/></a>
-                <div class="team-classic-caption">
-                  <h5 class="team-classic-name"><a href="#">Ryan Wilson</a></h5>
-                  <p class="team-classic-status">Founder</p>
-                </div>
-              </article>
-            </div>
-            <div class="col-md-6 col-lg-4 col-xl-4 wow fadeInRight" data-wow-delay=".1s">
-              <!-- Team Classic-->
-              <article class="team-classic"><a class="team-classic-figure" href="#"><img src="images/team-2-370x406.jpg" alt="" width="370" height="406"/></a>
-                <div class="team-classic-caption">
-                  <h5 class="team-classic-name"><a href="#">Jill Peterson</a></h5>
-                  <p class="team-classic-status">Garden Manager</p>
-                </div>
-              </article>
-            </div>
-            <div class="col-md-6 col-lg-4 col-xl-4 wow fadeInRight" data-wow-delay=".2s">
-              <!-- Team Classic-->
-              <article class="team-classic"><a class="team-classic-figure" href="#"><img src="images/team-3-370x406.jpg" alt="" width="370" height="406"/></a>
-                <div class="team-classic-caption">
-                  <h5 class="team-classic-name"><a href="#">Sam Robinson</a></h5>
-                  <p class="team-classic-status">Farmyard Coordinator</p>
-                </div>
-              </article>
-            </div>
-          </div>
-        </div>
-      </section>
+    
 
       <!-- About us-->
       <section class="section">
@@ -263,28 +208,28 @@
                   <div class="counter-creative">
                     <div class="counter-creative-number"><span class="counter">12</span><span class="icon counter-creative-icon fl-bigmug-line-trophy55"></span>
                     </div>
-                    <h6 class="counter-creative-title">Awards</h6>
+                    <h6 class="counter-creative-title">Premiações</h6>
                   </div>
                 </div>
                 <div class="col-sm-6 col-md-5 col-lg-3 wow fadeInLeft" data-wow-delay=".1s">
                   <div class="counter-creative">
                     <div class="counter-creative-number"><span class="counter">2</span><span class="symbol">k</span><span class="icon counter-creative-icon fl-bigmug-line-up104"></span>
                     </div>
-                    <h6 class="counter-creative-title">Products</h6>
+                    <h6 class="counter-creative-title">Produtos</h6>
                   </div>
                 </div>
                 <div class="col-sm-6 col-md-5 col-lg-3 wow fadeInLeft" data-wow-delay=".2s">
                   <div class="counter-creative">
                     <div class="counter-creative-number"><span class="counter">679</span><span class="icon counter-creative-icon fl-bigmug-line-sun81"></span>
                     </div>
-                    <h6 class="counter-creative-title">Happy Clients</h6>
+                    <h6 class="counter-creative-title">Clientes Satisfeitos</h6>
                   </div>
                 </div>
                 <div class="col-sm-6 col-md-5 col-lg-3 wow fadeInLeft" data-wow-delay=".3s">
                   <div class="counter-creative">
                     <div class="counter-creative-number"><span class="counter">13</span><span class="icon counter-creative-icon fl-bigmug-line-user143"></span>
                     </div>
-                    <h6 class="counter-creative-title">Farmers</h6>
+                    <h6 class="counter-creative-title">Lojas</h6>
                   </div>
                 </div>
               </div>
@@ -297,7 +242,7 @@
       <section class="section section-md bg-default section-top-image">
         <div class="container">
           <div class="oh h2-title">
-            <h2 class="wow slideInUp" data-wow-delay="0s">Our Gallery</h2>
+            <h2 class="wow slideInUp" data-wow-delay="0s">Alguns Produtos</h2>
           </div>
           <div class="row row-30" data-lightgallery="group">
             <div class="col-sm-6 col-lg-4">
