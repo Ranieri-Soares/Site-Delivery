96b5a8173d528cdf2e82d6f88d8791ff143ea8ba
Ranieri-Soares <ranieri_silvasoares@hotmail.com>
2021-01-08 23:28:08 -0300

    Scripts de mensagens

--- contatos.php
+++ contatos.php
@@ -19,7 +19,7 @@
                   </div>
                   <div class="col-sm-6">
                     <div class="form-wrap">
-                      <input class="form-input" id="contact-tel" type="telefone" name="telefone" >
+                      <input class="form-input" id="telefone" type="telefone" name="telefone" >
                       <label class="form-label" for="contact-tel">WhatsApp</label>
                     </div>
                   </div>
--- js/script.js
+++ js/script.js
@@ -923,13 +923,13 @@
 		if (plugins.rdMailForm.length) {
 			var i, j, k,
 				msg = {
-					'MF000': 'Successfully sent!',
-					'MF001': 'Recipients are not set!',
-					'MF002': 'Form will not work locally!',
-					'MF003': 'Please, define email field in your form!',
-					'MF004': 'Please, define type of your form!',
-					'MF254': 'Something went wrong with PHPMailer!',
-					'MF255': 'Aw, snap! Something went wrong.'
+					'MF000': 'Enviado com Sucesso!',
+					'MF001': 'Destinátario não Definido!',
+					'MF002': 'Erro ao Enviar!',
+					'MF003': 'Favor, insira o email no formulário!',
+					'MF004': 'Erro ao Enviar!',
+					'MF254': 'Erro ao Enviar!',
+					'MF255': 'Você não pode enviar email estando em servidor local.'
 				};
 
 			for (i = 0; i < plugins.rdMailForm.length; i++) {
@@ -997,7 +997,7 @@
 							form.addClass('form-in-process');
 
 							if (output.hasClass("snackbars")) {
-								output.html('<p><span class="icon text-middle fa fa-circle-o-notch fa-spin icon-xxs"></span><span>Sending</span></p>');
+								output.html('<p><span class="icon text-middle fa fa-circle-o-notch fa-spin icon-xxs"></span><span>Enviando</span></p>');
 								output.addClass("active");
 							}
 						} else {
